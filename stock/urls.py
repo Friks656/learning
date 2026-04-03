38f4b232be13d360c9531c3d77b27afe616ba86c
friks <friks@friks.com>
2026-04-04 02:25:32 +0300

    Prodazha

--- stock/urls.py
+++ stock/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from stock.views import stock_list, stock_detail, stock_buy, account
-
+from stock.views import stock_list, stock_detail, stock_buy, stock_sell, account
 
 urlpatterns = [
     path('list/', stock_list, name='list'),
     path('detail/<int:pk>/', stock_detail, name='detail'),
     path('buy/<int:pk>/', stock_buy, name='buy'),
-    path('account/', account, name='account')
+    path('sell/<int:pk>/', stock_sell, name='sell'),   # новый маршрут
+    path('account/', account, name='account'),
 ]
